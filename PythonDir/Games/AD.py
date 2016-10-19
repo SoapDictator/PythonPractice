e722606fac90721d8475df159e195a56e2d78692
Konstantin SoapDictator <soapdictator@gmail.com>
2016-10-20 02:20:49 +0300

    Added basic functionality tank class

--- PythonDir/Games/AD.py
+++ PythonDir/Games/AD.py
@@ -4,28 +4,49 @@
 import pygame, sys
 from pygame.locals import *
 
-global FPS = 30
-global windowWidth = 640
-global windowHeight = 480
-global cellSize = 20
+FPS = 1
+WINDOWWIDTH = 640
+WINDOWHEIGTH = 480
+CELLSIZE = 20
 
-assert windowWidth % cellSize == 0
-assert windowHeight % cellSize == 0
+assert WINDOWWIDTH % CELLSIZE == 0
+assert WINDOWHEIGTH % CELLSIZE == 0
 
-global cellWidth = int(windowWidth / cellSize)
-global cellHeight = int(windowWidth / cellSize)
+CELLWIDTH = int(WINDOWWIDTH / CELLSIZE)
+CELLHEIGTH = int(WINDOWHEIGTH / CELLSIZE)
 
 #             R    G    B
-global WHITE     = (255, 255, 255)
-global BLACK     = (  0,   0,   0)
-global RED       = (255,   0,   0)
-global GREEN     = (  0, 255,   0)
-global DARKGREEN = (  0, 155,   0)
-global DARKGRAY  = ( 40,  40,  40)
-global BGCOLOR = BLACK
+WHITE		= (255, 255, 255)
+BLACK		= (  0,	  0,   0)
+RED			= (155,   0,   0)
+DARKRED		= (255,   0,   0)
+GREEN		= (  0, 255,   0)
+DARKGREEN	= (  0, 155,   0)
+LIGHTGRAY	= (120, 120, 120)
+BGCOLOR		= BLACK
+
+TANKARRAY = []
+ARRAYPOSITION = 0
 
 class Window(object):
 	def __init__(self):
+		global FPSCLOCK, DISPLAYSURF, BASICFONT
+
+		pygame.init()
+		FPSCLOCK = pygame.time.Clock()
+		DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGTH))
+		BASICFONT = pygame.font.Font('freesansbold.ttf', 18)
+		pygame.display.set_caption('AD')
+		
+	def drawGrid(self):
+		for x in range(0, WINDOWWIDTH, CELLSIZE): # draw vertical lines
+			pygame.draw.line(DISPLAYSURF, LIGHTGRAY, (x, 0), (x, WINDOWHEIGTH))
+		for y in range(0, WINDOWHEIGTH, CELLSIZE): # draw horizontal lines
+			pygame.draw.line(DISPLAYSURF, LIGHTGRAY, (0, y), (WINDOWWIDTH, y))
+
+	def terminate(self):
+		pygame.quit()
+		sys.exit()
 		
 
 class Unit(object):
@@ -34,15 +55,98 @@ class Unit(object):
 	statHealth = 0
 	statArmor = 0
 	statDamage = 0
+	
+	def CreateUnit(self):
+		pass
+	
+	def DrawUnit(self):
+		pass
+		
+	def MoveUnit(self, toX, toY):
+		pass
 
 class Tank(Unit):
-	coordX = 0
-	coordY = 0
+	coordX = -1 * CELLSIZE
+	coordY = -1 * CELLSIZE
 	statHealth = 0
 	statArmor = 0
 	statDamage = 0
-	def __init__(self):
+	arrayPos = 0
+	
+	def CreateUnit(self, coordX, coordY): # new tank always needs to be the last one in the array
 		print("A new tank appears!")
-		print("Its health equals %s") % statHealth
+		global ARRAYPOSITION, TANKARRAY
+		TANKARRAY.append(self)
+		self.arrayPos = ARRAYPOSITION
+		ARRAYPOSITION += 1 
+		self.coordX = coordX * CELLSIZE
+		self.coordY = coordY * CELLSIZE
+	
+	def DestroyUnit(self): #problem: if there is only one tank the delete function doesn't work
+		global ARRAYPOSITION, TANKARRAY
+		self.coordX = -1 * CELLSIZE #resets the position just in case
+		self.coordY = -1 * CELLSIZE 
+		for TANK in TANKARRAY: #shifts all tanks overriding the destroyed one
+			if self.arrayPos > TANK.arrayPos:
+				TANK.arrayPos -= 1
+		ARRAYPOSITION -= 1
+		print("A tank has been erased!")
+	
+	def DrawUnit(self):
+		outerRect = pygame.Rect(self.coordX, self.coordY, CELLSIZE, CELLSIZE)
+		innerRect = pygame.Rect(self.coordX +4, self.coordY +4, CELLSIZE -8, CELLSIZE -8)
+		pygame.draw.rect(DISPLAYSURF, DARKRED, outerRect)
+		pygame.draw.rect(DISPLAYSURF, RED, innerRect)
 		
+	def MoveUnit(self, toX, toY):
+		self.coordX += toX * CELLSIZE
+		self.coordY += toY * CELLSIZE
+
+class Main(object):
+	def __init__(self):
+		Window0 = Window()
+		while True:
+			for event in pygame.event.get():
+				if event.type == QUIT:
+					self.terminate()
+				elif event.type == KEYDOWN:
+					if event.key == K_ESCAPE:
+						self.terminate()
+					elif event.key == K_DOWN:
+						TANKARRAY[0].MoveUnit(0, +1)
+					elif event.key == K_UP:
+						TANKARRAY[0].MoveUnit(0, -1)
+					elif event.key == K_RIGHT:
+						TANKARRAY[0].MoveUnit(+1, 0)
+					elif event.key == K_LEFT:
+						TANKARRAY[0].MoveUnit(-1, 0)
+					elif event.key == K_d:
+						TANKARRAY[1].DestroyUnit()
+	
+			DISPLAYSURF.fill(BGCOLOR)
+			Window0.drawGrid()
+			for TANK in TANKARRAY:
+				TANK.DrawUnit()
+			pygame.display.update()
+			FPSCLOCK.tick(FPS)
+			
+	def checkForKeyPress(self):
+		if len(pygame.event.get(QUIT)) > 0:
+			self.terminate()
+
+		keyUpEvents = pygame.event.get(KEYUP)
+		if len(keyUpEvents) == 0:
+			return None
+		if keyUpEvents[0].key == K_ESCAPE:
+			self.terminate()
+		return keyUpEvents[0].key
+		
+	def terminate(self):
+		pygame.quit()
+		sys.exit()
+
 Bulldozer = Tank()
+Bulldozer.CreateUnit(1, 1)
+Blinker = Tank()
+Blinker.CreateUnit(3, 1)
+StartShenanigans = Main()
