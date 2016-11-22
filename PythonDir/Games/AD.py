604c24ad16bb651721f234745270239a1f75a01e
SoapDictator <SoapDictator@gmail.com>
2016-11-22 23:23:01 +0300

    - Added a proper Event Queue - Added the unitAttack eventAdd TODO: - Fix input manager and replace the current system with a dictionary based one - Add a sorting mechanism to the event queue - Add a proper illustration of maximum travel distance and firing range of a unit - Add all the other unit types - Modify the event queue in order to sort the events properly for each unit type

--- PythonDir/Games/AD.py
+++ PythonDir/Games/AD.py
@@ -5,27 +5,83 @@ import pygame, sys
 from pygame.locals import *
 
 class GameEventManager(object):
-	EVEQUEUE = []
+	EVENTQUEUE = []
 	
-	def __init__(self):
-		print("this is GameEventManager __init__")
-		
-	def addEvent(self, event):
-		self.EVENTQUEUE.append(event)
-		
-	def removeEvent(self, event):
-		for index in range(0, len(self.EVENTQUEUE)-1):
-			if self.EVENTQUEUE[index] == event:
-				del EVENTQUEUE[index]
-				for k in range(index+1, len(self.EVENTQUEUE)-1):
-					self.EVENTQUEUE[k-1] = self.EVENTQUEUE[k]
+	def eventAdd(self, eventName, data):	#self, string, tuple
+		if eventName == "EventTerminate":
+			event = EventTerminate()
+			self.EVENTQUEUE.append(event)
+		elif eventName == "EventUnitCreate":
+			event = EventUnitCreate(data)
+			self.EVENTQUEUE.append(event)
+		elif eventName == "EventUnitDestroy":
+			event = EventUnitDestroy(data)
+			self.EVENTQUEUE.append(event)
+		elif eventName == "EventUnitHealthCheck":
+			event = EventUnitHealthCheck()
+			self.EVENTQUEUE.append(event)
+		elif eventName == "EventUnitMove":
+			event = EventUnitMove()
+			self.EVENTQUEUE.append(event)
+		elif eventName == "EventUnitAttack":
+			event = EventUnitAttack(data)
+			self.EVENTQUEUE.insert(0, event)
+		#self.EVENTQUEUE.append(event)
+	
+	def eventHandle(self):
+		self.eventAdd("EventUnitHealthCheck", 0)
+		for event in self.EVENTQUEUE:
+			event.execute()
+		del self.EVENTQUEUE[0:len(self.EVENTQUEUE)]
 
 class GameEvent(object):
-	def __init__(self):
+	def execute(self):
 		print("This is a prototype event, get out!")
 
-class InputManager(object):
-	inputState = ['moveSelection', 'unitSelected']
+class EventTerminate(GameEvent):
+	def execute(self):
+		print("Terminated")
+		pygame.quit()
+		sys.exit()
+
+class EventUnitCreate(GameEvent):
+	def __init__(self, coord):
+		self.coord = coord
+		
+	def execute(self):
+		print("A new tank appears!")
+		Unit0.unitCreate(self.coord[0], self.coord[1])
+
+class EventUnitDestroy(GameEvent):
+	def __init__(self, deletedTank):
+		self.deletedTank = deletedTank
+		
+	def execute(self):
+		print("A tank has been erased!")
+		Unit0.unitDestroy(self.deletedTank)
+
+class EventUnitHealthCheck(GameEvent):
+	def execute(self):
+		print("Health check!")
+		for Tank in Unit0.TANKARRAY:
+			if Tank.statHealth <= 0:
+				Event0.eventAdd("EventUnitDestroy", Tank)
+
+class EventUnitMove(GameEvent):
+	def execute(self):
+		Unit0.unitMove()
+
+class EventUnitAttack(GameEvent):
+	def __init__(self, Tanks):
+		self.attackingTank = Tanks[0]
+		self.targetedTank = Tanks[1]
+	
+	def execute(self):
+		print("A unit is under attack!")
+		Unit0.unitAttack(self.attackingTank, self.targetedTank)
+
+class InputManager(object):		#TODO: input binds for separate input states
+	inputState = ['moveSelection', 'unitSelected', 'unitAttack']
 	currentState = inputState[0]
 	
 	def getState(self):
@@ -35,7 +91,6 @@ class InputManager(object):
 		self.currentState = self.inputState[num]
 	
 	def handleInput(self):
-		global SELECTEDTANK		#fix this :\
 		for event in pygame.event.get():	#this is chinese level of programming, needs fixing as well
 			if event.type == QUIT:
 				self.terminate()
@@ -46,7 +101,7 @@ class InputManager(object):
 					elif event.key == K_DOWN:	Map0.moveSelect(0, +1)
 					elif event.key == K_RIGHT:	Map0.moveSelect(+1, 0)
 					elif event.key == K_LEFT:	Map0.moveSelect(-1, 0)
-					elif event.key == K_e:		#fix that as well
+					elif event.key == K_e:
 						Unit0.unitSelect()
 						if Unit0.SELECTEDTANK != -1:
 							self.setState(1)
@@ -54,15 +109,31 @@ class InputManager(object):
 				if event.type == KEYDOWN:
 					if event.key == K_ESCAPE:	self.setState(0)
 					elif event.key == K_UP:		Unit0.moveStore(0, -1)
-					elif event.key == K_DOWN:	Unit0.moveStore(0, +1)
+					elif event.key == K_DOWN:	Unit0.moveStore(0, +1) #getattr(Unit0, "moveStore")(0, +1)
 					elif event.key == K_RIGHT:	Unit0.moveStore(+1, 0)
 					elif event.key == K_LEFT:	Unit0.moveStore(-1, 0)
-					elif event.key == K_e:		Unit0.moveUnit()
-								
+					elif event.key == K_e:		
+						Event0.eventAdd("EventUnitMove", 0)
+						Event0.eventHandle()
+					elif event.key == K_a:		self.setState(2)
+			elif self.getState() == 'unitAttack':
+				if event.type == KEYDOWN:
+					if event.key == K_ESCAPE:	self.setState(1)
+					elif event.key == K_UP:		Map0.moveSelect(0, -1)
+					elif event.key == K_DOWN:	Map0.moveSelect(0, +1)
+					elif event.key == K_RIGHT:	Map0.moveSelect(+1, 0)
+					elif event.key == K_LEFT:	Map0.moveSelect(-1, 0)
+					elif event.key == K_e:		
+						targetedTank = Map0.mapGetUnit()
+						if targetedTank != -1:
+							Event0.eventAdd("EventUnitAttack", [Unit0.SELECTEDTANK, targetedTank])
+							self.setState(1)
+
 	def terminate(self):
+		print("Terminated")
 		pygame.quit()
 		sys.exit()
-
+	
 class DrawingManager(object):
 	FPS = 0
 	FPSCLOCK = 0
@@ -153,7 +224,7 @@ class MapManager(object):
 				return TANK
 		return -1
 	
-	def moveSelect(self, toX, toY):
+	def moveSelect(self, toX, toY):		#ducttape till I get the cursor support running
 		self.MAPSELECT.statCoord[0] += toX*Window0.CELLSIZE
 		self.MAPSELECT.statCoord[1] += toY*Window0.CELLSIZE
 		
@@ -164,7 +235,6 @@ class UnitManager(object):
 	SELECTEDTANK = 0
 	
 	def unitCreate(self, toX, toY): 	# new tank always will be the last one in the array
-		print("A new tank appears!")
 		NewTank = Tank()
 		self.TANKARRAY.append(NewTank)
 		NewTank.arrayPos = len(self.TANKARRAY)-1
@@ -175,19 +245,18 @@ class UnitManager(object):
 	def unitDestroy(self, deletedTank):
 		del self.TANKARRAY[deletedTank.arrayPos]
 		del self.MOVEQUEUE[deletedTank.arrayPos]
-		for TANK in self.TANKARRAY: 	#shifts all tanks so the new tank will be last in the array
+		for TANK in self.TANKARRAY: 	#shifts all tanks situated after the deleted one in the array
 			if deletedTank.arrayPos < TANK.arrayPos:
 				TANK.arrayPos -= 1
-		print("A tank has been erased!")
 		
-	def moveStore(self, toX, toY):
+	def moveStore(self, toX, toY):		#saves a move and checks for the previos move undo
 		moveLength = len(self.MOVEQUEUE[self.SELECTEDTANK.arrayPos])
 		if self.MOVEQUEUE[self.SELECTEDTANK.arrayPos][moveLength-1] == [-toX, -toY]:
 			del self.MOVEQUEUE[self.SELECTEDTANK.arrayPos][moveLength-1]
 		else:
 			self.MOVEQUEUE[self.SELECTEDTANK.arrayPos].append([toX, toY])
 		
-	def moveUnit(self):
+	def unitMove(self):
 		for TANK in self.TANKARRAY:
 			for step in range(1, len(self.MOVEQUEUE[TANK.arrayPos])):
 				TANK.statCoord[0] += self.MOVEQUEUE[TANK.arrayPos][step][0]*Window0.CELLSIZE
@@ -198,9 +267,13 @@ class UnitManager(object):
 		for TANK in self.TANKARRAY:
 			self.MOVEQUEUE[TANK.arrayPos][0] = [TANK.statCoord[0], TANK.statCoord[1]]
 			del self.MOVEQUEUE[TANK.arrayPos][1:len(self.MOVEQUEUE[TANK.arrayPos])]
+	
+	def unitAttack(self, attackingTank, targetedTank):
 		
+		targetedTank.statHealth -= attackingTank.statDamage
+	
 	def unitSelect(self):
-		self.SELECTEDTANK = Map0.mapGetUnit()
+		self.SELECTEDTANK = Map0.mapGetUnit()		
 		
 class Unit(object):
 	statCoord = [0, 0]
@@ -213,10 +286,11 @@ class Unit(object):
 
 class Tank(Unit):
 	statCoord = [0, 0]
-	statHealth = 0
+	statHealth = 10
 	statArmor = 0
-	statDamage = 0
+	statDamage = 10
 	statSpeed = 5
+	statFR = 2
 	arrayPos = 0
 	
 	def drawUnit(self):
@@ -227,23 +301,25 @@ class Tank(Unit):
 		
 class Main(object):
 	def __init__(self):
-		global Window0, Input0, Unit0, Map0
-		#global MoveUp, MoveDown, MoveRight, MoveLeft	#rudiment from the commmand prompt
+		global Event0, Window0, Input0, Unit0, Map0
+		Event0 = GameEventManager()
 		Window0 = DrawingManager()
 		Input0 = InputManager()
 		Unit0 = UnitManager()
 		Map0 = MapManager()
 		
-		Unit0.unitCreate(1, 1)
-		Unit0.unitCreate(3, 1)
+		self.test()
 		
 		while True:
 			Input0.handleInput()
+			#Event0.eventHandle()
 			Window0.screenRefresh()
 			Window0.FPSCLOCK.tick(Window0.FPS)
-		
-	def terminate(self):
-		pygame.quit()
-		sys.exit()
+	
+	def test(self):
+		Event0.eventAdd("EventUnitCreate", [1, 1])
+		Event0.eventAdd("EventUnitCreate", [3, 1])
+		Event0.eventHandle()
+		#Event0.eventAdd("EventTerminate")		#in case things go wrong
 
 StartShenanigans = Main()
